ad488b045f56d19f5deb0720f07eefefd47b18cb
Ikhsan Syahri Ramadhan <masari311003@gmail.com>
2025-03-23 00:55:15 +0700

    fix: update database config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('notes', 'root', '', {
+const db = new Sequelize('notes', 'root', '123220024', {
     host: '34.121.20.199',
     dialect: 'mysql',
 });
